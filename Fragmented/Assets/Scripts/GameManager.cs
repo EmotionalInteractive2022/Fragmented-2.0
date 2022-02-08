34624d7f74a7b0230a97c03f5767679d2a4f98c5
EmotionalInteractive2022 <97882975+EmotionalInteractive2022@users.noreply.github.com>
2022-02-08 15:04:28 +0000

    la lu lei lei lo

---
+++ Fragmented/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int score = 0;
+
+    private static GameManager _instance;
+
+    public static GameManager Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
