667225314333f8217169d165414013e81c9a0d0a
EmotionalInteractive2022 <97882975+EmotionalInteractive2022@users.noreply.github.com>
2022-02-10 14:14:43 +0000

    loading level works

--- Fragmented/Assets/Scripts/Collectable.cs
+++ Fragmented/Assets/Scripts/Collectable.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Collectable : MonoBehaviour
-{
+
+public class Collectable : MonoBehaviour { 
+
+    public static int fragmentCount = 1; 
+
     // Start is called before the first frame update
     void OnTriggerEnter(Collider thing)
     {
@@ -12,12 +15,15 @@ public class Collectable : MonoBehaviour
         {
             Destroy(gameObject);
             GameManager.Instance.score += 1;
+            if (fragmentCount >= 1)
+                SceneManager.LoadScene(1);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-         
+        
+          
     }
 }
