3c30406bae20c2820a59e3c382965f7670e88a08
EmotionalInteractive2022 <97882975+EmotionalInteractive2022@users.noreply.github.com>
2022-02-11 15:56:43 +0000

    ,hgkmyjytjx

---
+++ Fragmented/Assets/sc2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechanger : MonoBehaviour
+{
+    public void onCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
