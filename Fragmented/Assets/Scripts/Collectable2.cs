d391a95d6e97a0fe00d175d591dc0c774300f265
EmotionalInteractive2022 <97882975+EmotionalInteractive2022@users.noreply.github.com>
2022-02-11 09:37:48 +0000

    ain menu and crewdits

--- Fragmented/Assets/Scripts/Collectable2.cs
+++ Fragmented/Assets/Scripts/Collectable2.cs
@@ -1,30 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
 
-public class Collectable : MonoBehaviour { 
+//public class Collectable : MonoBehaviour { 
 
-    public static int fragmentCount = 1; 
+    //public static int fragmentCount = 1; 
 
     // Start is called before the first frame update
-    void OnTriggerEnter(Collider thing)
-    {
-        if (thing.tag == "Player")
-        {
-            Destroy(gameObject);
-            GameManager.Instance.score += 1;
-            if (fragmentCount >= 1)
-                SceneManager.LoadScene(1);
-        }
-    }
+    //void OnTriggerEnter(Collider thing)
+    //{
+       // if (thing.tag == "Player")
+        //{
+          //  Destroy(gameObject);
+            //GameManager.Instance.score += 1;
+            //if (fragmentCount >= 1)
+              //  SceneManager.LoadScene(1);
+        //}
+    //
 
     // Update is called once per frame
-    void Update()
-    {
+    //void Update()
+    //{
         
           
-    }
-}
+    //}
+//}
 
