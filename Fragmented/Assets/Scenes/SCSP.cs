c136208d30ff00bea530e486556b01ab50619983
EmotionalInteractive2022 <97882975+EmotionalInteractive2022@users.noreply.github.com>
2022-02-22 15:04:09 +0000

    la lu lei lei lo

--- Fragmented/Assets/Scenes/SCSP.cs
+++ Fragmented/Assets/Scenes/SCSP.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SC2 : MonoBehaviour
+public class SCSP : MonoBehaviour
 {
     public void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(5);
+        SceneManager.LoadScene(6);
     }
 }
--- Fragmented/Assets/Scripts/MouseLook.cs
+++ Fragmented/Assets/Scripts/MouseLook.cs
@@ -21,7 +21,7 @@ public class MouseLook : MonoBehaviour
     void Update()
     {
         float mouseX = Input.GetAxis ("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis ("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis ("Mouse Y") * -mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
